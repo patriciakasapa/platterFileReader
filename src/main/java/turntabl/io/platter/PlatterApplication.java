9e659bf3fd33ed0403c5a04bc18fbf4472e4ccfc
patriciakasapa <patriciakasapa07@gmail.com>
2019-12-10 13:52:43 +0000

    Platter using jdbc

---
+++ src/main/java/turntabl/io/platter/PlatterApplication.java
@@ -0,0 +1,13 @@
+package turntabl.io.platter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/turntabl/io/platter/PlatterApplicationTests.java
@@ -0,0 +1,13 @@
+package turntabl.io.platter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
